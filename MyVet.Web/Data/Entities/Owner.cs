c2e13b94e12bcba9ad3b49716ff346b627d2ce6c
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2019-12-05 10:27:25 -0500

    InitialDb

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+    }
+}
