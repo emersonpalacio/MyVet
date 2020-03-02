9d6096d9336cb0bd1ed088b0fe28acda4128dfad
emersonpalaciootalvaro <emersonpalaciootalvaro@gmail.com>
2020-03-02 12:45:30 -0500

    IntialPublich

--- MyVet.Web/Startup.cs
+++ MyVet.Web/Startup.cs
@@ -71,8 +71,6 @@ namespace MyVet.Web
             });
 
 
-
-
             services.AddTransient<SeedDb>();
             services.AddScoped<IUserHelper, UserHelper>();
             services.AddScoped<ICombosHelper, CombosHelper>();
